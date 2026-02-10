cc58cdab712f058e33bc2374b3025c1dddb5d91a
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2026-02-10 17:54:29 -0300

    test: create validate cpf test

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,17 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each(["97456321558", "71428793860", "87748248800"])(
+  "Deve testar um cpf válido: %s",
+  (cpf: string) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(true);
+  },
+);
+
+test.each(["11111111111", "111", "111111111111", null, undefined])(
+  "Deve testar um cpf inválido: %s",
+  (cpf: any) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(false);
+  },
+);
