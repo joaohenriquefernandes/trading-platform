7d595bc27c28e20930991803a2338a9cb9010d88
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2026-02-10 23:35:23 -0300

    test: create main test

---
+++ test/main.test.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+test('Deve criar uma conta', async () => {
+  const input = {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    document: '97456321558',
+    password: 'asdQWE123'
+  }
+  const responseSignup = await axios.post('http://localhost:3000/signup', input);
+  const outputSignup = responseSignup.data;
+  expect(outputSignup.accountId).toBeDefined();
+  const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`);
+  const outputGetAccount = responseGetAccount.data;
+  expect(outputGetAccount.name).toBe(input.name);
+  expect(outputGetAccount.email).toBe(input.email);
+  expect(outputGetAccount.document).toBe(input.document);
+  expect(outputGetAccount.password).toBe(input.password);
+});
