a61ede73db2d89a7c3eac90a574087b15557905e
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2026-03-06 00:05:49 -0300

    feat: create frontend app

---
+++ frontend/vue/src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+  const form = ref({
+    name: '',
+    email: '',
+    document: '',
+    password: '',
+    accountId: '',
+    message: ''
+  });
+
+  async function signup() {
+    const input = form.value;
+    const response = await fetch('http://localhost:3000/signup', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(input)
+    });
+    const output = await response.json();
+    if (output.accountId) {
+      form.value.accountId = output.accountId;
+      form.value.message = 'success'
+    } else {
+      form.value.message = output.message;
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <div>
+      <input class="input-name" type="text" v-model="form.name" placeholder="Name">
+    </div>
+    <div>
+      <input class="input-email" type="text" v-model="form.email" placeholder="Email">
+    </div>
+    <div>
+      <input class="input-document" type="text" v-model="form.document" placeholder="Document">
+    </div>
+    <div>
+      <input class="input-password" type="text" v-model="form.password" placeholder="Password">
+    </div>
+    <button @click="signup()" class="button-signup">Signup</button>
+    <span class="span-account-id">{{form.accountId}}</span>
+    <span class="span-message">{{form.message}}</span>
+  </div>
+</template>
+
+<style scoped>
+</style>
